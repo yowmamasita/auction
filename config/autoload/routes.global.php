0d6b78f38ceae6d23f882b3cd00f55a7c209db3a
Ben Sarmiento <me@bensarmiento.com>
2017-01-08 07:49:28 +0800

    Fix namespace on route config

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -4,10 +4,10 @@ return [
     'dependencies' => [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
-            App\Action\PingAction::class => App\Action\PingAction::class,
+            Auction\Action\PingAction::class => Auction\Action\PingAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            Auction\Action\HomePageAction::class => Auction\Action\HomePageFactory::class,
         ],
     ],
 
@@ -15,13 +15,13 @@ return [
         [
             'name' => 'home',
             'path' => '/',
-            'middleware' => App\Action\HomePageAction::class,
+            'middleware' => Auction\Action\HomePageAction::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'api.ping',
             'path' => '/api/ping',
-            'middleware' => App\Action\PingAction::class,
+            'middleware' => Auction\Action\PingAction::class,
             'allowed_methods' => ['GET'],
         ],
     ],
