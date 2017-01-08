641ff16db5a282ec7bbbb74c720558d1253b07ff
Ben Sarmiento <me@bensarmiento.com>
2017-01-08 19:11:40 +0800

    Add BidPack and Wallet entity for tracking bid return value

---
+++ src/Auction/Domain/Entity/BidPack.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 08/01/2017
+ * Time: 7:05 PM
+ */
+
+namespace Auction\Domain\Entity;
+
+
+class BidPack
+{
+
+}
---
+++ src/Auction/Domain/Entity/Wallet.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 08/01/2017
+ * Time: 7:06 PM
+ */
+
+namespace Auction\Domain\Entity;
+
+
+class Wallet
+{
+
+}
